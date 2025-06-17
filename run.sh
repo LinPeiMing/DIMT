8bd66d0db3e53adc719ac3d9890a1ce0976502b0
Peiming Lin <137766846+LinPeiMing@users.noreply.github.com>
2025-06-17 23:52:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4329 --use-env\
-# basicsr/train.py -opt options/train/DIMT.yml --launcher pytorch
-
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4329 --use-env\
- basicsr/train.py -opt options/train/DIMT-GAN.yml --launcher pytorch
+basicsr/train.py -opt options/train/DIMT.yml --launcher pytorch
+
+# CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4329 --use-env\
+#  basicsr/train.py -opt options/train/DIMT-GAN.yml --launcher pytorch
