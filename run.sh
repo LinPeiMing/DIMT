8b51ff7a1c9de6733290b36a1723b053b36bbacf
Peiming Lin <137766846+LinPeiMing@users.noreply.github.com>
2025-06-17 23:52:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,3 @@
-#CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4328 --use-env \
-#basicsr/train.py -opt options/train/NAFSSR.yml --launcher pytorch
-
-# CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4328 \
-# basicsr/train.py -opt options/train/NAFSSR_FT.yml --launcher pytorch
-
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=4328 \
-# basicsr/train.py -opt options/train/NAFSSR_FT_GAN.yml --launcher pytorch
-
-# CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4329 --use-env\
-# basicsr/train.py -opt options/train/BlindSR.yml --launcher pytorch
-
 #CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4329 --use-env\
 # basicsr/train.py -opt options/train/DIMT.yml --launcher pytorch
 
