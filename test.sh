c6f74d1120f81f622ea384c2c8c25a4a428c84c1
Peiming Lin <137766846+LinPeiMing@users.noreply.github.com>
2025-06-17 23:52:59 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,2 @@
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4323 --use-env \
-basicsr/test.py -opt options/test/swinFIRSSR.yml --launcher pytorch
-
-#CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4327 --use-env \
-#basicsr/test.py -opt options/test/NAFSSR.yml --launcher pytorch
-
-#CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4323 --use-env \
-#basicsr/test.py -opt options/test/test_SCGLANet-L_4x_Track3.yml --launcher pytorch
+basicsr/test.py -opt options/test/DIMT-GAN.yml --launcher pytorch
